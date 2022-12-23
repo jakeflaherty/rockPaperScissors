a65d7415247a28b658b7297c758618d60ce6248e
Jake Flaherty <flahertyjake@gmail.com>
2022-12-23 12:37:16 -0500

    RPS game updated with images, effects, event listeners

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+// to grab your score container
+const humanScore = document.querySelector('.playerPoints.playerScore');
+
+// to grab computer's score container
+const compScore1 = document.querySelector('.computerPoints.computerScore');
+
+// to grab result container
+const result = document.querySelector('.resultsContainer');
+
+
+
 // generate random number between min/max inclusively
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
@@ -18,71 +29,106 @@ else {
 
 }
 
-// play one round of rps
-function playRound(playerSelection, computerSelection) {
-    const player = playerSelection.toLowerCase();
-    while (playerScore < 5 && compScore < 5){
-        if (player === computerSelection) {
-            return "It's a tie!";
-        }
-        if (player === "rock") {
-            if (computerSelection === "paper") {
-                compScore++;
-                return "Paper covers rock! The computer wins this round...";
-            }
-            else if (computerSelection === "scissors") {
-                playerScore++;
-                 return "Rock beats scissors! The player wins this round...";
-            }
-        }
-        if (player === "paper") {
-            if (computerSelection === "rock") {
-                playerScore++;
-                return "Paper covers rock! The player wins this round...";
-            }
-            else if (computerSelection === "scissors") {
-                compScore++;
-                return "Scissors cuts paper! The computer wins this round...";
-            }
-        }
-        if (player === "scissors") {
-            if (computerSelection === "rock") {
-                compScore++;
-                return "Rock breaks scissors! The computer wins this round...";
-            }
-            else if (computerSelection === "paper") {
-                playerScore++;
-                return "Scissors cuts paper! The player wins this round...";
-            }
-        }
-    } 
-    console.log(playerScore, compScore);
-
-    
-    
-}
-
 let playerScore = 0;
 let compScore = 0;
 
+// play one round of rps
+function playRound(playerSelection, computerSelection) {
+    //const player = playerSelection.toLowerCase();
+    let drawPrompt = "Draw... Try again!";
+    if (playerScore < 5 && compScore < 5) {
+        if (playerSelection === computerSelection) {
+            result.textContent = drawPrompt;
+            //return "It's a tie!";
+        }
+        if (playerSelection === "rock") {
+            if (computerSelection === "paper") {
+                compScore++;
+                result.textContent = "Paper covers rock! The computer wins this round...";
+                compScore1.textContent = compScore;
+                //return "Paper covers rock! The computer wins this round...";
+            }
+            else if (computerSelection === "scissors") {
+                playerScore++;
+                result.textContent = "Rock beats scissors! The player wins this round...";
+                humanScore.textContent = playerScore;
+                 //return "Rock beats scissors! The player wins this round...";
+            }
+        }
+        if (playerSelection === "paper") {
+            if (computerSelection === "rock") {
+                playerScore++;
+                result.textContent = "Paper covers rock! The player wins this round...";
+                humanScore.textContent = playerScore;
+                //return "Paper covers rock! The player wins this round...";
+            }
+            else if (computerSelection === "scissors") {
+                compScore++;
+                result.textContent = "Scissors cuts paper! The computer wins this round...";
+                compScore1.textContent = compScore;
+                //return "Scissors cuts paper! The computer wins this round...";
+            }
+        }
+        if (playerSelection === "scissors") {
+            if (computerSelection === "rock") {
+                compScore++;
+                result.textContent = "Rock breaks scissors! The computer wins this round...";
+                compScore1.textContent = compScore;
+                //return "Rock breaks scissors! The computer wins this round...";
+            }
+            else if (computerSelection === "paper") {
+                playerScore++;
+                result.textContent = "Scissors cuts paper! The player wins this round...";
+                humanScore.textContent = playerScore;
+                //return "Scissors cuts paper! The player wins this round...";
+            }
+        }
+        console.log(playerScore, compScore);
+    }
 
-const btn1 = document.querySelector('#btn1');
-btn1.addEventListener('click', function (e) {
-console.log(e);
-console.log(playRound(e.target.innerHTML,getComputerChoice()));
+    if (playerScore == 5) {
+        result.textContent = "You win";
+        button.style.visibility = 'visible';
+    } else if (compScore == 5) {
+        result.textContent = "Computer wins";
+        button.style.visibility = 'visible';
+    }
+        
+    } 
+    
+
+
+const rock = document.querySelector('#btn1');
+rock.addEventListener('click', () => {
+playerSelection = 'rock';
+computerSelection = getComputerChoice();
+playRound(playerSelection, computerSelection);
 });
 
-const btn2 = document.querySelector('#btn2');
-btn2.addEventListener('click', function (e) {
-console.log(e);
-console.log(playRound(e.target.innerHTML,getComputerChoice()));
+const paper = document.querySelector('#btn2');
+paper.addEventListener('click', () => {
+playerSelection = 'paper';
+computerSelection = getComputerChoice();
+playRound(playerSelection, computerSelection);
 });
 
-const btn3 = document.querySelector('#btn3');
-btn3.addEventListener('click', function (e) {
-console.log(e);
-console.log(playRound(e.target.innerHTML,getComputerChoice()));
+const scissors = document.querySelector('#btn3');
+scissors.addEventListener('click', () => {
+playerSelection = 'scissors';
+computerSelection = getComputerChoice();
+playRound(playerSelection, computerSelection);
 });
+
+// clicking button
+const button = document.querySelector('.again');
+button.addEventListener('click', () => {
+    playerScore = 0;
+    humanScore.textContent = playerScore;
+    compScore = 0;
+    compScore1.textContent = compScore;
+    result.textContent = "";
+    button.style.visibility = 'hidden';
+})
 
 
 
